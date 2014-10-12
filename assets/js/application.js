88365613b9882301b688f1a182267b6ec0a055c3
Laju Morrison <morrelinko@gmail.com>
2014-10-12 20:51:43 +0100

    Updated Register user UI

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,24 @@
-(function($) {
+(function ($) {
 
-  $(function() {
+  $(function () {
+    // Create user page
+    $('#input-user-type').on('change', function (e) {
+      var $this = $(this);
+      var $input_unique_id = $('#input-unique-id');
+
+      switch ($this.val()) {
+        case 'staff':
+          $input_unique_id.attr('placeholder', 'Enter staff id here...');
+          break;
+        case 'student':
+          $input_unique_id.attr('placeholder', 'Enter student\'s matric number here...');
+          break;
+        default:
+          $input_unique_id.attr('placeholder', 'Select a user type first...');
+          break;
+      }
+    });
+
     // Manage users page
     $('.delete-user').on('click', function (e) {
       var $this = $(this);
@@ -13,8 +31,8 @@
         url: app.base_url + 'ajax/users/' + id,
         type: 'delete',
         dataType: 'json',
-        success: function(res) {
-          $container.fadeOut('slow').done(function() {
+        success: function (res) {
+          $container.fadeOut('slow').done(function () {
             $container.remove();
           });
         }
